5164a1a7fea75a97bd2319ded57e3c5d2cefae1c
Arshdeep Singh <singh_arsy@yahoo.com>
2016-07-28 20:31:28 -0400

    added questions to vampire file

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you? what year were you born in?"
+input = gets.chomp
+
+puts " Our company cafeteria serves garlic bread. should we order some for you? (yes/no)"
+input2 = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? (yes/no)"
+input3 = gets.chomp
+
+
+
+

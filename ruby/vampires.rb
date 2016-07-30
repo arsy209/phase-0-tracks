53a2b8799c297c1db0a1c98e6b90481a7cb5e458
Arshdeep Singh <singh_arsy@yahoo.com>
2016-07-30 17:38:33 -0400

    changed and deleted some writing

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,45 +1,57 @@
 
-	
+
 puts "What is your name?"
 name = gets.chomp
-
+if name == " drake cula" 
+	correct_name = false
+	elsif name == " tu fang"
+	correct_name = false
+else 
+	current_name = true
+end
 puts "How old are you?"
 age = gets.chomp
-
 
 puts "What year were you born"
 born = gets.chomp
 current_year = 2016
 correct_age = current_year - born.to_i
-puts " you are #{current_year-born.to_i} old"
 
-if born == age
-	correct_age = true
-elsif born != age
-    correct_age = false 
-end
-puts " Our company cafeteria serves garlic bread. should we order some for you? (yes/no)"
+puts " you are actually #{current_year - born.to_i} years old"
+			if correct_age = age
+				correct_age = true
+			elsif correct_age != age
+    			correct_age = false 
+			end
+
+puts " Our company cafeteria serves garlic bread. should we order some for you (yes/no)?"
 garlic = gets.chomp
 
- if garlic == "yes"
- 	likes_garlic = true
- else 
- 	likes_garlic = false 
- end
-puts "Would you like to enroll in the company's health insurance? (yes/no)"
+ 		if garlic =="yes"
+ 			likes_garlic = true
+ 		else
+ 			likes_garlic = false 
+ 		end
+
+
+puts "Would you like to enroll in the company's health insurance (yes/no)? "
 health = gets.chomp
-	if health == "yes"
-		wants_health_insurance = true
-	else 
-		wants_health_insurance = false 
-end	
+		if health == "yes"
+			wants_health_insurance = true
+		else
+			wants_health_insurance = false 
+		end	
 		
 	
-		
+	
 if correct_age && likes_garlic && wants_health_insurance
-	puts "probably not a vampire."
-elsif
-  puts "correct_Age"
-  elsif
- puts "Results inconclusive."
+	p "probably not a vampire."
+
+elsif correct_age && (likes_garlic || wants_health_insurance)
+	 p "Probably a vampire."
+elsif  correct_name && correct_age && likes_garlic && wants_health_insurance
+ 	p "Definitely a vampire."
+else
+ 	p "Results inconclusive."
+
 end

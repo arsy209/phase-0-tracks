d88c3e5a281c9d20e17cebcc358790b0f1bd4c7b
Arshdeep Singh <singh_arsy@yahoo.com>
2016-07-30 15:38:56 -0400

    added true and false to questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,17 +1,45 @@
+
+	
 puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
 age = gets.chomp
 
-puts= "what year were you born in?"
 
+puts "What year were you born"
+born = gets.chomp
+current_year = 2016
+correct_age = current_year - born.to_i
+puts " you are #{current_year-born.to_i} old"
+
+if born == age
+	correct_age = true
+elsif born != age
+    correct_age = false 
+end
 puts " Our company cafeteria serves garlic bread. should we order some for you? (yes/no)"
 garlic = gets.chomp
 
+ if garlic == "yes"
+ 	likes_garlic = true
+ else 
+ 	likes_garlic = false 
+ end
 puts "Would you like to enroll in the company's health insurance? (yes/no)"
 health = gets.chomp
-
-
-
-
+	if health == "yes"
+		wants_health_insurance = true
+	else 
+		wants_health_insurance = false 
+end	
+		
+	
+		
+if correct_age && likes_garlic && wants_health_insurance
+	puts "probably not a vampire."
+elsif
+  puts "correct_Age"
+  elsif
+ puts "Results inconclusive."
+end

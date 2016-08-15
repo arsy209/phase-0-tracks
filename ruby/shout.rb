c77fa98f5cf02f9ee99f90eeb92718d52d1d0774
Arshdeep Singh <singh_arsy@yahoo.com>
2016-08-15 10:31:20 -0400

    added two new classes with new information being printed out from angry mom and happy kid

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,19 +1,45 @@
-module Shout
+#module Shout
   # we'll put some methods here soon, but this code is fine for now!
 
 
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+  
 
-def self.yelling_happily(words)
+ #def self.yell_angrily(words)
+   # words + "!!!" + " :("
+ # end
 
-	words + " this is the besttt " " ;)"
+#def self.yelling_happily(words)
 
-end
-end
+	#words + " this is the besttt " " ;)"
+
+#end
+#end
 
 #driver code
 
-puts Shout.yell_angrily("YOOO")
-puts Shout.yelling_happily("HEYYY")
+#puts Shout.yell_angrily("YOOO")
+#puts Shout.yelling_happily("HEYYY")
+
+
+ 
+module Shout
+ def lecture(words)
+ 	words + ",im telling your dad !!!!!!!"
+ end
+
+ def happy(words)
+ 	words + " ,You are the best!"
+end
+end
+class Angry_mom
+	include Shout
+end
+
+class Happy_kid
+	include Shout
+end
+
+mom = Angry_mom.new
+p mom.lecture("how dare you not listen to me")
+kid =  Happy_kid.new
+p kid.happy("I LOVE THIS NEW TOY")

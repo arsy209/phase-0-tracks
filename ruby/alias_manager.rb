0497dddcca1dd085db8eff18fcd04f331cd253d6
Arshdeep Singh <singh_arsy@yahoo.com>
2016-09-01 14:20:49 -0400

    Update alias_manager.rb
    edited the spacing

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -22,39 +22,34 @@ new_new_name=[ ]
 #maps out each of the splitted letter
 new_name_split.map! do |letter|
 
-				if vowels.include?(letter)
-			   	   new_new_name << vowels[vowels.index(letter)+1]
-				elsif alphabet.include?(letter)
-					new_new_name << alphabet[alphabet.index(letter)+1]
-				elsif letter == " "
-					new_new_name << " "
-			    end
-end
-				new_new_name.join("").split.map{|letter| letter.capitalize}.join(" ")
+	if vowels.include?(letter)
+		new_new_name << vowels[vowels.index(letter)+1]
+	elsif alphabet.include?(letter)
+		new_new_name << alphabet[alphabet.index(letter)+1]
+	elsif letter == " "
+		new_new_name << " "
+	end
+    end
+	new_new_name.join("").split.map{|letter| letter.capitalize}.join(" ")
 end
 
 
 #driver code
 secret_names={}
 while true
-			print "\nInput your first name here:"
-				  first_name=gets.chomp.downcase
-			print "\nInput your last name here:"
-				  last_name=gets.chomp.downcase
-		 		  new_name =last_name+" "+first_name
-			print "\nYour new assigned secret name is:\n " + decode(new_name)
+	print "\nInput your first name here:"
+	      first_name=gets.chomp.downcase
+	print "\nInput your last name here:"
+	      last_name=gets.chomp.downcase
+	      new_name =last_name+" "+first_name
+	print "\nYour new assigned secret name is:\n " + decode(new_name)
 
-
-			print " \n\nYou can enter quit or continue by pressing enter the button."
-		 		  choice = gets.chomp
-	      
+	print " \n\nYou can enter quit or continue by pressing enter the button."
+	      choice = gets.chomp
 	      		if choice == "quit"
-  
- 				break
-
-
-				end
-			secret_names[last_name.capitalize+","+first_name.capitalize] = decode(new_name)
+ 			   break
+			end
+	secret_names[last_name.capitalize+","+first_name.capitalize] = decode(new_name)
 end
 
 #this would store each of the names and then print it out at the end after the user press quit
@@ -62,4 +57,4 @@ name = (decode(new_name))
 secret_names.store(new_name,name)
 
 secret_names.each  {|name, new_name| 
-	puts "#{new_name} is also known as #{name}."}
+                puts "#{new_name} is also known as #{name}."}

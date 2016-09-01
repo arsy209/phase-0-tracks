6c059d12ce22560e04af6551f244118c775b4cb1
Arshdeep Singh <singh_arsy@yahoo.com>
2016-09-01 14:16:46 -0400

    added pesudocoding to the file

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,7 +1,16 @@
+#create a method that would go through each of the letters of the alphabet for the inputted word
+#first it would split the word and allow the method to run through each of the letters
+#after the method is done, it would joined the splitted word back together
+#if it reaches a certain vowel, it would switch it with another vowel and continue on till it reaches the end of the word
+#if no vowels found then it would go through the alphabet and change words with the next letter in the alphabet
+#add a driver code which would allow the user to input their first and last name and print out their new secret name after running it through the method
+
+
+
 puts "WELCOME TO THE NAME CHANGER PROGRAM:\n"
 
 
-
+#this method would run through the inputted name and print out their new name
 def decode(new_name)
 
 vowels="aeiou"
@@ -10,6 +19,7 @@ alphabet="bcdfghjklmnpqrstvwxyz"
 new_name_split=new_name.split("")
 new_new_name=[ ]
 
+#maps out each of the splitted letter
 new_name_split.map! do |letter|
 
 				if vowels.include?(letter)
@@ -24,7 +34,7 @@ end
 end
 
 
-
+#driver code
 secret_names={}
 while true
 			print "\nInput your first name here:"
@@ -47,7 +57,7 @@ while true
 			secret_names[last_name.capitalize+","+first_name.capitalize] = decode(new_name)
 end
 
-
+#this would store each of the names and then print it out at the end after the user press quit
 name = (decode(new_name))
 secret_names.store(new_name,name)
 

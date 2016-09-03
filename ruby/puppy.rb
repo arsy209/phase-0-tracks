bbbcfdf7cf46479e245273de5f028181ab79f9ee
Arshdeep Singh <singh_arsy@yahoo.com>
2016-09-02 20:05:24 -0400

    added a puppy and its different features and characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+
+#animal : puppy
+
+
+
+#characteristics--------
+#fur color : white
+#eye color : blue
+#name : Thunder
+#height: 5 inches tall
+#type: pitbull
+
+#behavior----------
+#pout
+#eat
+#sleep
+#run
+#fetch
+#howl
+#licks toilet
+#gets excited fast

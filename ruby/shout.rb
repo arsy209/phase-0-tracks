0fa65f44c11176d2b0bf7e1195dc8ea2cb3fc490
Arshdeep Singh <singh_arsy@yahoo.com>
2016-09-05 13:12:23 -0400

    added a new module inside the class method

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,35 @@
 module Shout
 
-   def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+   #def self.yell_angrily(words)
+        #words + "!!!" + " :("
+   #end
 
-  	def self.yelling_happily(words)
-	words + " this is the besttt " " ;)"
-	end
+  	#def self.yelling_happily(words)
+		#words + " this is the besttt " " ;)"
+	#end
 
+#end
+
+#puts Shout.yell_angrily("YOOO")
+#puts Shout.yelling_happily("HEYYY")
+
+ def lecture(words)
+ 	words + ",im telling your dad !!!!!!!"
+ end
+
+ def happy(words)
+ 	words + " ,You are the best!"
+end
+end
+class Angry_mom
+	include Shout
 end
 
-puts Shout.yell_angrily("YOOO")
-puts Shout.yelling_happily("HEYYY")
+class Happy_kid
+	include Shout
+end
+
+mom = Angry_mom.new
+p mom.lecture("how dare you not listen to me")
+kid =  Happy_kid.new
+p kid.happy("I LOVE THIS NEW TOY")

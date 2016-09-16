85a70eb17a2fbd40a50d3a2af3280d12beca5843
Arshdeep Singh <singh_arsy@yahoo.com>
2016-09-15 21:28:12 -0400

    added a method that would reverse a given word or sentence and also a boolean value to see if 1 == 1 is true or not

---
+++ js/javascript-intro-lab/explore.js
@@ -0,0 +1,33 @@
+
+
+//pseudocode
+
+//1.takes a word and reverses it
+//2.prints out the results using console.log()
+//3. puts a number 1 == 1 and prints true
+
+
+
+function reverse(word) {
+	var new_word = ""
+	for (var a = word.length - 1; a >= 0; a--){
+		new_word += word[a]
+	}
+	if (word === new_word) 
+	{
+		return
+	}
+	
+	return new_word
+}
+
+
+// driver code
+// create new variable for changed string
+
+var reversed_name = reverse("Arsh")
+var reversed_clan = reverse("Coyotes")
+console.log(reversed_name)
+console.log(reversed_clan)
+console.log(1 == 1)
+console.log(2 == 1)

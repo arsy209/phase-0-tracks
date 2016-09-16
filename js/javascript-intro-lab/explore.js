4cb4cad7024c5070f9ee3cba49d3e40b5a6d5590
Arshdeep Singh <singh_arsy@yahoo.com>
2016-09-15 21:34:28 -0400

    added missing  semi-colons

--- js/javascript-intro-lab/explore.js
+++ js/javascript-intro-lab/explore.js
@@ -1,5 +1,4 @@
 
-
 //pseudocode
 
 //1.takes a word and reverses it
@@ -9,25 +8,25 @@
 
 
 function reverse(word) {
-	var new_word = ""
+	var new_word = "";
 	for (var a = word.length - 1; a >= 0; a--){
-		new_word += word[a]
+		new_word += word[a];
 	}
 	if (word === new_word) 
 	{
-		return
+		return;
 	}
 	
-	return new_word
+	return new_word;
 }
 
 
 // driver code
 // create new variable for changed string
 
-var reversed_name = reverse("Arsh")
-var reversed_clan = reverse("Coyotes")
-console.log(reversed_name)
-console.log(reversed_clan)
-console.log(1 == 1)
-console.log(2 == 1)
+var reversed_name = reverse("Arsh");
+var reversed_clan = reverse("Coyotes");
+console.log(reversed_name);
+console.log(reversed_clan);
+console.log(1 == 1);
+console.log(2 == 1);

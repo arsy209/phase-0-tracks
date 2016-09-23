122386aedf871f3971d0d15b8908c5f3d2ed3cf2
Arshdeep Singh <singh_arsy@yahoo.com>
2016-09-22 20:22:12 -0400

    first let the file know that sqlite3 is require for the file to run correctly. Made a method that would insert the columns of the correct information into the new table and then execute it. Made another method which would insert the information into the table by creating new rows each time

--- databases/club_application/application.rb
+++ databases/club_application/application.rb
@@ -10,3 +10,28 @@
 #At the end, the database file would open which would contain all the information that the user answered from the question
 
 
+#the gem that is required
+require 'sqlite3'
+
+#initiates new database file
+db = SQLite3::Database.new("club.db")
+#creates a new table for every new informaton that would be inputted.
+table = <<-SQL 
+CREATE TABLE IF NOT EXISTS applications(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		age  VARCHAR(255),
+		phone_number INT,
+		address INT,
+		email VARCHAR(255),
+		club_name VARCHAR(255),
+		hours INT
+)
+SQL
+#initiates the new table.
+db.execute(table)
+	#adds all the information to the coresponding columns in the table
+	def applications(db,name,age,phone_number,address,email,club_name,hours)
+	
+		db.execute("INSERT INTO applications (name,age,phone_number,address,email,club_name,hours) VALUES (?,?,?,?,?,?,?)", [name,age,phone_number,address,email,club_name,hours])
+	end

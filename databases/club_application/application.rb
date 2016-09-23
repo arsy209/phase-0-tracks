d6d8a7ac9d734bca4fb4a83bbed97cf5a957bb34
Arshdeep Singh <singh_arsy@yahoo.com>
2016-09-22 20:18:12 -0400

    made minor changes to the pesudo code

--- databases/club_application/application.rb
+++ databases/club_application/application.rb
@@ -1,12 +1,12 @@
 #8.5- combining Ruby and SQL
 #Arshdeep Singh
 
-#make a method that would create a application for people to apply to a club
-#start off by requiring the gems needed for the file to work correctly
-#create a new db and a table inside it with the correct amount of columns and rows needed
-#the db should be able to execute and insert the information inside the table 
-#create a user input where the user would be asked different questions for the application
-#as the user answers each questions, it would be stored into the database file 
-#at the end, the database file would open which would contain all the information that the user answered from the question
+#Make a method that would create a application for people to apply to a club
+#Start off by requiring the gems needed for the file to work correctly
+#Create a new db and a table inside it with the correct amount of columns and rows needed
+#The db should be able to execute and insert the information inside the table 
+#Create a user input where the user would be asked different questions for the application
+#As the user answers each questions, it would be stored into the database file 
+#At the end, the database file would open which would contain all the information that the user answered from the question
 
 

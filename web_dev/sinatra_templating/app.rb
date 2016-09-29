bd83ee7870b3c6803cec5b64c67deb938887186d
Arshdeep Singh <singh_arsy@yahoo.com>
2016-09-29 16:37:17 -0400

    New erb template and form to collect different campus reviews, had a error with a loop after user inputs the data

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -5,7 +5,9 @@ require 'sqlite3'
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
+db2 = SQLite3::Database.new("campus.db")
 db.results_as_hash = true
+db2.results_as_hash = true
 
 # show students on the home page
 get '/' do
@@ -13,9 +15,18 @@ get '/' do
   erb :home
 end
 
+get'/' do
+@campus = db2.execute("SELECT * FROM campus")
+erb :home
+end
+
 get '/students/new' do
   erb :new_student
 end
+get '/students/campus' do
+  erb :campus
+end
+
 
 # create new students via
 # a form
@@ -23,5 +34,11 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+post '/campus' do
+  db2.execute("INSERT INTO campus (name, campus, review) VALUES (?,?,?)", [params['name'], params['campus'], params['review']])
+  redirect '/'
+end
+
 
 # add static resources
